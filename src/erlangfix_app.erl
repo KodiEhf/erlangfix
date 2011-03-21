620f9e18b907524983169377e4b547d89b4d2801
Dagur Gunnarsson <dagur@snake.vodafone>
2011-03-21 22:05:05 +0000

    commit

---
+++ src/erlangfix_app.erl
@@ -0,0 +1,16 @@
+-module(erlangfix_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    erlangfix_sup:start_link().
+
+stop(_State) ->
+    ok.
